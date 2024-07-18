8f7935cca9fb4d447af4ba25b8cfbf8a29c82cee
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-18 11:20:42 +0800

    Add JSON test data loading and parsing

--- rust/wsgup/src/main.rs
+++ rust/wsgup/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::error::Error;
+use json;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Read test data from file
+    let message: String = fs::read_to_string("../../shared/testdata.json")?;
+    //println!("{}", message);
+    
+    // Parse JSON test data
+    let res = json::parse(message.as_str()).unwrap();
+    
+    println!("iv           = {}", res["body"]["iv"]);
+    println!("enc_userid   = {}", res["body"]["enc_userid"]);
+    println!("tag_userid   = {}", res["body"]["tag_userid"]);
+    println!("userid       = {}", res["body"]["userid"]);
+    println!("enc_password = {}", res["body"]["enc_password"]);
+    println!("tag_password = {}", res["body"]["tag_password"]);
+    
+    Ok(())
 }
