5ece83547ef8238f16012f5b94aa3f56e8c6d279
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-18 12:18:38 +0800

    Add ccm and aes crate. Non-working example commented out.

--- rust/wsgup/src/main.rs
+++ rust/wsgup/src/main.rs
@@ -2,6 +2,18 @@ use std::fs;
 use std::error::Error;
 use json;
 
+/*
+use aes::Aes128;
+use ccm::{
+    aead::{KeyInit, OsRng, generic_array::GenericArray}, //Unused: Aead, 
+    consts::{U10, U13},
+    Ccm,
+};
+
+// AES-128-CCM type with tag and nonce size equal to 10 and 13 bytes respectively
+pub type Aes128Ccm = Ccm<Aes128, U10, U13>;
+*/
+
 fn main() -> Result<(), Box<dyn Error>> {
     // Read test data from file
     let message: String = fs::read_to_string("../../shared/testdata.json")?;
@@ -16,6 +28,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("userid       = {}", res["body"]["userid"]);
     println!("enc_password = {}", res["body"]["enc_password"]);
     println!("tag_password = {}", res["body"]["tag_password"]);
+
+    // Untested example not compiling. 
+    // error[E0283]: type annotations needed for `&GenericArray<u8, N>`
+    // Source: https://docs.rs/ccm/latest/ccm/
+    
+    /*
+    let key = Aes128Ccm::generate_key(&mut OsRng);
+    let cipher = Aes128Ccm::new(&key);    
+    let nonce = GenericArray::from_slice(b"unique nonce."); // 13-bytes; unique per message
+    let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())?;
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())?;
+    assert_eq!(&plaintext, b"plaintext message");
+    */
     
     Ok(())
 }
