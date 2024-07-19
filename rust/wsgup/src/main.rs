943710fad155de99955eeffc88d2f3b2687ed9f9
gyk4j <147011991+gyk4j@users.noreply.github.com>
2024-07-19 12:53:32 +0800

    Add working example with successful decryption of test data

--- rust/wsgup/src/main.rs
+++ rust/wsgup/src/main.rs
@@ -3,19 +3,17 @@ use std::error::Error;
 use std::str;
 use json;
 
-//use base64::prelude::*;
-
 use chrono::{NaiveDate}; // DateTime, Local, 
 
 use aes::Aes128;
 use ccm::{
-    aead::{Aead, KeyInit, OsRng, generic_array::GenericArray},  
+    aead::{Aead, KeyInit, generic_array::GenericArray}, // OsRng,   
     consts::{U16, U12},
     Ccm,
 };
 
 
-// AES-128-CCM type with tag and nonce size equal to 10 and 13 bytes respectively
+// AES-128-CCM type with tag and nonce size equal to 16 and 12 bytes respectively
 pub type Aes128Ccm = Ccm<Aes128, U16, U12>;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -48,11 +46,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("enc_password = {}", res["body"]["enc_password"]);
     println!("tag_password = {}", res["body"]["tag_password"]);
     */
-
-    /*
-    let iv = BASE64_STANDARD.decode(res["body"]["iv"].as_str().unwrap()).unwrap_or_default();
-    println!("==> iv           = {:?}", iv);
-    */
+    
+    // Build the decryption key
     
     /*
     let now: DateTime<Local> = Local::now();
@@ -73,6 +68,28 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let key = hex::decode(key_hex).unwrap();
     
+    // Prepare to decrypt user ID and password
+    let cipher = Aes128Ccm::new_from_slice(&key).unwrap();
+    let nonce_str: String = res["body"]["iv"].as_str().unwrap().to_string();
+    let nonce: &GenericArray<u8, U12> = GenericArray::from_slice(nonce_str.as_bytes());
+    
+    // Decrypt user ID
+    let enc_userid = res["body"]["enc_userid"].as_str().unwrap().to_string();
+    let tag_userid = res["body"]["tag_userid"].as_str().unwrap().to_string();
+    let userid_tag_hex = format!("{enc_userid}{tag_userid}");
+    let userid_tag_bin = hex::decode(userid_tag_hex).unwrap();
+    let userid = cipher.decrypt(nonce, &userid_tag_bin[..]).unwrap();
+    println!("User ID  = {}", str::from_utf8(&userid).unwrap());
+    
+    // Decrypt password
+    let enc_password = res["body"]["enc_password"].as_str().unwrap().to_string();
+    let tag_password = res["body"]["tag_password"].as_str().unwrap().to_string();
+    let password_tag_hex = format!("{enc_password}{tag_password}");
+    let password_tag_bin = hex::decode(password_tag_hex).unwrap();
+    let password = cipher.decrypt(nonce, &password_tag_bin[..]).unwrap();
+    println!("Password = {}", str::from_utf8(&password).unwrap());
+    
+    /*
     let key = Aes128Ccm::generate_key(&mut OsRng);
     let cipher = Aes128Ccm::new(&key);    
     let nonce: &GenericArray<u8, U12> = GenericArray::from_slice(b"unique nonce");
@@ -82,6 +99,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     assert_eq!(&decrypted, b"plaintext message");
     
     println!("Decrypted = {}", str::from_utf8(&decrypted).unwrap());
+    */
     
     Ok(())
 }
